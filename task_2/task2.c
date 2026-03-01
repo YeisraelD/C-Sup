585eb55fd3522b383e20575d3f43bab72465b078
YeisraelD <Yeisraeld@gmail.com>
2026-03-01 13:02:37 +0300

    declearing and assigning variables

---
+++ task_2/task2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int integer = 5;
+    float floatvar = 5.0;
+    char string[] = "hello u";
+
+    printf("%d is integer\n", integer);
+    printf("%f is float\n", floatvar);
+    printf("%s is string\n", string);
+}
