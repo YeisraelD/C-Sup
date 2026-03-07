a4e18ce69618a19ce35e77165cb26a21dcb765bb
YeisraelD <Yeisraeld@gmail.com>
2026-03-07 11:01:32 +0300

    task 10/ memory view of sizeof()

---
+++ task_10/task10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int array[10] = {0, 1, 2,3, 4,5,6,7,8,9};
+
+    printf("the array has %d elements.", sizeof(array)/sizeof(array[0]));
+    
+    return 0;
+}
---
+++ task_9/task9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+  
+  if(argc < 3 || argc > 3)
+  {
+  	printf("Usage: ./task9 Firstname Lastname");
+  }
+  else
+  {
+  	printf("Hello, %s %s", argv[9], argv[9]);
+  }
+  return 0;
+}
