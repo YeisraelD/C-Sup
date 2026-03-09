3fc1dc1c79186559a4496cc55eddf410ec4f9278
YeisraelD <Yeisraeld@gmail.com>
2026-03-09 10:49:12 +0300

    producer function

---
+++ OS process synchronization/prodCons_semaphores.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define N 100
+typedef int semaphore;
+semaphore mutex = 1;
+semaphore full = 0;
+semaphore empty = N;
+
+void producer(void){
+    int item;
+
+    while (true){
+        item = producer_item();    // creat item 
+        down(&empty);      // wait for empty slot , if ibuffer is full producer waits automatically
+        down(&mutex);    // enter critical section, lock
+        insert_item(item);
+        up(&mutex);     //unlock, leave cs
+        up(&full);     // ++ count of full slot , this signals the consumer that an item exists
+
+    }
+}
