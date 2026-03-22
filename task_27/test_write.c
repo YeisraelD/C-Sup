c7c669ddb7a42f16abeea491a2cb944e4882c402
YeisraelD <Yeisraeld@gmail.com>
2026-03-22 12:06:03 +0300

    Created a program to verify the hook.

---
+++ task_27/test_write.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * This program writes "Hello, World!" to stdout.
+ * When run with LD_PRELOAD=./task27.so, the output should be hooked.
+ */
+
+int main(void) {
+  write(1, "Hello, World!", 13);
+  write(1, "\n", 1);
+  return 0;
+}
